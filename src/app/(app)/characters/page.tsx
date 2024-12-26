db324a987d8d443d63ebdfddc36e5d03f4d9b95b
Kerem Karatal <kkaratal@gmail.com>
2024-12-26 18:06:59 +0100

    feat: (WIP) add add/remove buttons to the character list

--- src/app/(app)/characters/page.tsx
+++ src/app/(app)/characters/page.tsx
@@ -7,9 +7,7 @@ export default async function CharactersPage() {
   return (
     <div className="flex h-full">
       <CharacterList characters={characters} />
-      <div className="flex-1 p-6">
-        {/* Future content will go here */}
-      </div>
+      <div className="flex-1 p-6">{/* Future content will go here */}</div>
     </div>
   );
 }
