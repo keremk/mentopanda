2695599eb13cb7db460d86e2d8ca783c0afb5796
Kerem Karatal <kkaratal@gmail.com>
2025-01-07 15:28:23 +0100

    fix: policy for storage buckets

--- supabase/migrations/20241222155721_create_storage_policies.sql
+++ supabase/migrations/20241222155721_create_storage_policies.sql
@@ -1,17 +1,14 @@
+-- Create buckets
 insert into storage.buckets (id, name, public)
-values ('avatars', 'avatars', true);
+values ('avatars', 'avatars', true),
+    ('trainings', 'trainings', true);
 
+-- Create a single policy that handles both buckets
 CREATE POLICY "Allow authenticated uploads" ON storage.objects FOR
 INSERT TO authenticated WITH CHECK (
-        bucket_id = 'avatars'
-        AND auth.uid() = owner
-    );
-
-insert into storage.buckets (id, name, public)
-values ('trainings', 'trainings', true);
-
-CREATE POLICY "Allow authenticated uploads" ON storage.objects FOR
-INSERT TO authenticated WITH CHECK (
-        bucket_id = 'trainings'
+        (
+            bucket_id = 'avatars'
+            OR bucket_id = 'trainings'
+        )
         AND auth.uid() = owner
     );
