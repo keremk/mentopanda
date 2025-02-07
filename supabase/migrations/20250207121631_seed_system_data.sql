4c296f28b23a229eb0bb9072d880ef30b974af0e
Kerem Karatal <kkaratal@gmail.com>
2025-02-07 13:57:40 +0100

    fix: fix the type

--- supabase/migrations/20250207121631_seed_system_data.sql
+++ supabase/migrations/20250207121631_seed_system_data.sql
@@ -18,7 +18,10 @@ end;
 $$ language plpgsql;
 
 -- System Permissions
-create or replace function insert_role_permissions(role_name text, permissions text []) returns void as $$ begin if not exists (
+create or replace function insert_role_permissions(
+        role_name user_role,
+        permissions app_permission [] -- Changed to app_permission[] type
+    ) returns void as $$ begin if not exists (
         select 1
         from role_permissions
         where role = role_name
@@ -47,36 +50,36 @@ perform setval('organizations_id_seq', 3, false);
 
 -- System Roles and Permissions
 perform insert_role_permissions(
-    'admin',
+    'admin'::user_role,
     ARRAY [
-        'training.manage',
-        'training.make.public',
-        'enrollment.manage',
-        'user.select',
-        'user.admin',
-        'organization.admin'
+        'training.manage'::app_permission,
+        'training.make.public'::app_permission,
+        'enrollment.manage'::app_permission,
+        'user.select'::app_permission,
+        'user.admin'::app_permission,
+        'organization.admin'::app_permission
     ]
 );
 
 perform insert_role_permissions(
-    'manager',
+    'manager'::user_role,
     ARRAY [
-        'training.manage',
-        'enrollment.manage',
-        'user.select'
+        'training.manage'::app_permission,
+        'enrollment.manage'::app_permission,
+        'user.select'::app_permission
     ]
 );
 
 perform insert_role_permissions(
-    'member',
+    'member'::user_role,
     ARRAY [
-        'enrollment.manage'
+        'enrollment.manage'::app_permission
     ]
 );
 
 -- Clean up helper functions
 drop function if exists insert_system_org(int, text, text);
 
-drop function if exists insert_role_permissions(text, text []);
+drop function if exists insert_role_permissions(user_role, app_permission []);
 
 end $$;
