fc02957d650b4b1b65fae2ccd6a6d9ae48323691
Kerem Karatal <kkaratal@gmail.com>
2025-02-08 19:06:48 +0100

    fix: second try for mobile issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+        />
       </head>
       <body
         className={cn(
