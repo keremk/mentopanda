2c816f9aab016c1450046d7abcf848f4a29b0c0c
Kerem Karatal <kkaratal@gmail.com>
2025-05-20 13:36:20 +0200

    fix: fixes the styling and tooltip for feedback button

--- src/components/feedback-button.tsx
+++ src/components/feedback-button.tsx
@@ -12,7 +12,8 @@ export function FeedbackButton() {
     <SidebarMenu>
       <SidebarMenuItem>
         <SidebarMenuButton
-          className="flex items-center"
+          tooltip="Give us your feedback"
+          className="flex items-center text-sidebar-foreground/60"
           id="posthog-feedback-button" // Unique ID for PostHog to target
         >
           <MessageSquarePlus className="h-[1.2rem] w-[1.2rem]" />
