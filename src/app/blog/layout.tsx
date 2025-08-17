0629c142933d15c256b4ee135ef773a5258e6263
Kerem Karatal <kkaratal@gmail.com>
2025-08-17 14:44:09 +0200

    fix: starting to add some docs, new layout and using the styling from the app

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,11 +1,12 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <HomeLayout {...baseOptions}>
+    <DocsLayout {...baseOptions} tree={source.pageTree}>
       {children}
-    </HomeLayout>
+    </DocsLayout>
   );
 }
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,15 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    ...components,
+    Accordion,
+    Accordions,
+    img: (props) => <ImageZoom {...(props as any)} />,
+    // Add any additional components you want to use in MDX here
+    ...components, // Allow overriding or extending with custom components
   };
 }
